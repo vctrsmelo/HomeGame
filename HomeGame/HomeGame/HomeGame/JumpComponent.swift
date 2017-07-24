00a6e7d16173b6e8f203ae2289381a882c451211
Victor S Melo <victor.contas@outlook.com>
2017-07-24 13:27:03 -0300

    control system upgraded

--- HomeGame/HomeGame/HomeGame/JumpComponent.swift
+++ HomeGame/HomeGame/HomeGame/JumpComponent.swift
@@ -28,7 +28,6 @@ class JumpComponent: GKComponent {
     
     func jumpAnimate(){
         
-        print("vamo pulaa")
         if rightMovement{
             player.jump(positionDirection: .right )
         }
