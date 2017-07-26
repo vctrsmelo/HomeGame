279e779d4e3d9ecedd3232473eed72e5b89d10d8
Bhárbara <khyna.babi@gmail.com>
2017-07-26 16:59:24 -0300

    MainScene ready and Credits scene being finished

--- HomeGame/HomeGame/HomeGame/CreditsScene.swift
+++ HomeGame/HomeGame/HomeGame/CreditsScene.swift
@@ -11,14 +11,82 @@ import GameplayKit
 
 class CreditsScene: SKScene {
     
+    let creditsSceneLaura = false
     
+    var newBackground: SKSpriteNode!
+    var sea:        SKSpriteNode!
+    var stone:      [SKSpriteNode]!
     
     var background = SKSpriteNode()
     var nameArray = ["Bharbara Cechin", "Douglas Gehring", "Juliana Cardoso", "Laura Corssac", "Victor Melo"]
     
     override func didMove(to view: SKView) {
         
+        if(creditsSceneLaura){
+            didMoveLaura()
+        } // fim credits scene laura
+        else {
+            if(initCreditsSceneAttributes()){
+                print("Credits SUCCESS")
+                
+                self.addChild(self.newBackground)
+                self.addChild(self.sea)
+//                for stone in stone {
+//                    self.addChild(stone)
+//                }
+            }
+        }
         
+    }
+    
+    func initCreditsSceneAttributes() -> Bool {
+        initNewBackgroundAndSea()
+        
+        return true
+    }
+    
+    func initNewBackgroundAndSea() {
+        /* Initialize background with texture */
+        // init BACKGROUND
+        let backgroundTexture = SKTexture(imageNamed: "Prancheta 6")
+        
+        let viewWidth = self.view?.bounds.width
+        let viewHeight = self.view?.bounds.height
+        let backgroundSize = CGSize(width: viewWidth!, height: viewHeight!)
+        
+        self.newBackground = SKSpriteNode(texture: backgroundTexture, size: backgroundSize)
+        
+        self.newBackground.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.newBackground.zPosition = CGFloat(-100)
+        
+        // init SEA
+        let seaTexture = SKTexture(imageNamed: "Prancheta 5")
+        
+        let seaSize = CGSize(width: viewWidth!, height: (viewHeight!)/4.5)
+        
+        self.sea = SKSpriteNode(texture: seaTexture, size: seaSize)
+        
+        self.sea.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        
+        let seaPosX = self.newBackground.frame.midX
+        let seaPosY = self.newBackground.frame.minY
+        self.sea.position = CGPoint(x: seaPosX, y: seaPosY)
+        
+        self.sea.zPosition = CGFloat(-90)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(creditsSceneLaura){
+            touchesBeganLaura(touches)
+        } // fim credits scene laura
+        else {
+            
+        }
+    }
+    
+    // CREDITS LAURA
+    
+    func didMoveLaura() {
         background = SKSpriteNode.init(imageNamed: "backGroundCredits")
         addChild(background)
         background.scale(to: (self.view?.frame.size)!)
@@ -30,9 +98,9 @@ class CreditsScene: SKScene {
             
             let info = SKLabelNode(fontNamed: "Helvetica-Bold")
             let info2 = SKLabelNode(fontNamed: "Helvetica-Bold")
-
+            
             let nameComps = name.components(separatedBy: " ")
-            let stringFormatted = String.init(format: nameComps[0] + "\r\n" + nameComps[1])
+            //let stringFormatted = String.init(format: nameComps[0] + "\r\n" + nameComps[1])
             
             info.text = nameComps[0]
             info2.text = nameComps[1]
@@ -43,14 +111,12 @@ class CreditsScene: SKScene {
             
             let iceberg = childNode(withName: "obj\(counter)") as! SKSpriteNode
             
-
             let icePos = CGPoint(x: iceberg.position.x - 3, y: iceberg.position.y + iceberg.frame.size.height/2 - 25)
             
             info.position = CGPoint(x: icePos.x, y: icePos.y)
             info2.position = CGPoint(x: icePos.x, y: icePos.y - 15)
             info.zPosition = 3
             info2.zPosition = 3
-
             
             let shadow = SKSpriteNode.init(imageNamed: "685")
             shadow.position = CGPoint(x: iceberg.position.x, y: iceberg.position.y - 3)
@@ -60,8 +126,6 @@ class CreditsScene: SKScene {
             shadow.alpha = 0.25
             shadow.blendMode = .alpha
             
-        
-            
             addChild(info2)
             addChild(shadow)
             addChild(info)
@@ -70,43 +134,29 @@ class CreditsScene: SKScene {
             iceberg.shadowedBitMask = 1
             iceberg.shadowCastBitMask = 1
             iceberg.lightingBitMask = 1
-            /*
-            let light = SKLightNode()
             
-            light.falloff = 0
-            light.isEnabled = true
-            light.shadowColor = .black
-            light.position = iceberg.position
-            light.zPosition = iceberg.zPosition + 1
-            light.categoryBitMask = 1
-            addChild(light)
-            */
+            /*
+             let light = SKLightNode()
+             
+             light.falloff = 0
+             light.isEnabled = true
+             light.shadowColor = .black
+             light.position = iceberg.position
+             light.zPosition = iceberg.zPosition + 1
+             light.categoryBitMask = 1
+             addChild(light)
+             */
         }
-        
-        
-        
-        
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
+    func touchesBeganLaura(_ touches: Set<UITouch>) {
         let woodTrunk = childNode(withName: "woodTrunk")
         let touch = touches.first
         
-        
         if (woodTrunk?.contains((touch?.location(in: self))!))!{
-            
             if let home = SKScene(fileNamed: "MainScene"){
                 self.view?.presentScene(home)
             }
-            
-            
         }
-        
-        
     }
-    
-    
-
 }
--- HomeGame/HomeGame/HomeGame/MainScene.swift
+++ HomeGame/HomeGame/HomeGame/MainScene.swift
@@ -12,6 +12,9 @@ import GameplayKit
 class MainScene: SKScene {
     
     var background: SKSpriteNode!
+    var sea:        SKSpriteNode!
+    var iceberg:    SKSpriteNode!
+    var stone:      SKSpriteNode!
     var playbtn:    SKSpriteNode!
     var creditsbtn: SKSpriteNode!
     
@@ -28,6 +31,9 @@ class MainScene: SKScene {
             print("Init Sucess!")
             
             self.addChild(self.background)
+            self.addChild(self.sea)
+            self.addChild(self.iceberg)
+            self.addChild(self.stone)
             self.addChild(self.playbtn)
             self.addChild(self.creditsbtn)
         }
@@ -35,6 +41,7 @@ class MainScene: SKScene {
     
     func initMainSceneAttributes() -> Bool {
         initBackground()
+        initMiddleground()
         initPlayBtn()
         initCreditsBtn()
         
@@ -42,25 +49,77 @@ class MainScene: SKScene {
     }
     
     func initBackground() {
-        /* Initialize background with a texture */
-        let backgroundTexture = SKTexture(imageNamed: "worldbackground")
+        /* Initialize background with texture */
+        let backgroundTexture = SKTexture(imageNamed: "Prancheta 2")
+
         let viewWidth = self.view?.bounds.width
         let viewHeight = self.view?.bounds.height
         let backgroundSize = CGSize(width: viewWidth!, height: viewHeight!)
+        
         self.background = SKSpriteNode(texture: backgroundTexture, size: backgroundSize)
         
         self.background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.background.zPosition = CGFloat(-100)
     }
     
+    func initMiddleground() {
+        /* Initialize middleground with texture*/
+        // init SEA
+        let seaTexture = SKTexture(imageNamed: "Prancheta 5")
+        
+        let viewWidth = self.view?.bounds.width
+        let viewHeight = self.view?.bounds.height
+        let seaSize = CGSize(width: viewWidth!, height: (viewHeight!)/4.5)
+        
+        self.sea = SKSpriteNode(texture: seaTexture, size: seaSize)
+        
+        self.sea.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        
+        let seaPosX = self.background.frame.midX
+        let seaPosY = self.background.frame.minY
+        self.sea.position = CGPoint(x: seaPosX, y: seaPosY)
+        
+        self.sea.zPosition = CGFloat(-90)
+        
+//        // init ICEBERG
+        let icebergTexture = SKTexture(imageNamed: "iceberg")
+        
+        let icebergSize = CGSize(width: (viewWidth!)/4.6, height: (viewHeight!)/1.9)
+        
+        self.iceberg = SKSpriteNode(texture: icebergTexture, size: icebergSize)
+        
+        self.iceberg.anchorPoint = CGPoint(x: 1.0, y: 0.0)
+        
+        let icebergPosX = self.background.frame.maxX
+        let icebergPosY = self.background.frame.minY
+        self.iceberg.position = CGPoint(x: icebergPosX, y: icebergPosY)
+        
+        self.iceberg.zPosition = CGFloat(-80)
+        
+//        // init STONE
+        let stoneTexture = SKTexture(imageNamed: "Stone_Front")
+        
+        let stoneSize = CGSize(width: (viewWidth!)/4, height: (viewHeight!)/6.8)
+        
+        self.stone = SKSpriteNode(texture: stoneTexture, size: stoneSize)
+        
+        self.stone.anchorPoint = CGPoint(x: -0.05, y: 0.0)
+        
+        let stonePosX = self.background.frame.minX
+        let stonePosY = self.background.frame.minY
+        self.stone.position = CGPoint(x: stonePosX, y: stonePosY)
+        
+        self.stone.zPosition = CGFloat(-80)
+    }
+    
     func initPlayBtn() {
         /* Initialize play button */
-        let playBtnTexture = SKTexture(imageNamed: "photo")
-        let playBtnSize = CGSize(width: 200, height: 100)
+        let playBtnTexture = SKTexture(imageNamed: "Button_play")
+        let playBtnSize = CGSize(width: 150, height: 60)
         
         self.playbtn = SKSpriteNode(texture: playBtnTexture, size: playBtnSize)
         
-        self.playbtn.anchorPoint = CGPoint(x: 1.2, y: 0.0)
+        self.playbtn.anchorPoint = CGPoint(x: 1.01, y: -0.2)
         
         let playbtnPosX = background.frame.midX
         let playbtnPosY = background.frame.minY
@@ -71,12 +130,12 @@ class MainScene: SKScene {
     
     func initCreditsBtn() {
         /* Initialize credits button */
-        let creditsBtnTexture = SKTexture(imageNamed: "photo")
-        let creditsBtnSize = CGSize(width: 200, height: 100)
+        let creditsBtnTexture = SKTexture(imageNamed: "Button_credits")
+        let creditsBtnSize = CGSize(width: 150, height: 60)
         
         self.creditsbtn = SKSpriteNode(texture: creditsBtnTexture, size: creditsBtnSize)
         
-        self.creditsbtn.anchorPoint = CGPoint(x: -0.2, y: 0.0)
+        self.creditsbtn.anchorPoint = CGPoint(x: -0.01, y: -0.2)
         
         let creditsbtnPosX = background.frame.midX
         let creditsbtnPosY = background.frame.minY
@@ -88,13 +147,13 @@ class MainScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // teste se apertou em playbtn ou creditsbtn
         let gamescene = SKScene(fileNamed: "Level1Scene")
-        let creditsScene = SKScene(fileNamed: "CreditsScene")
+        let creditsScene = SKScene(fileNamed: "NewCreditsScene")
         
         if self.playbtn.contains((touches.first?.location(in: self))!){
             self.view?.presentScene(gamescene)
         }
         else if self.creditsbtn.contains((touches.first?.location(in: self))!){
-            self.view?.presentScene(creditsScene)
+            self.view?.presentScene(creditsScene!, transition: SKTransition.fade(with: .white, duration: 1.5))
         }
     }
 }
